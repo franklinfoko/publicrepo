b330f87dc02a5aa880f45d2ba8b8e7d32c89824b
FranklinFoko <fokofranklin47@gmail.com>
2022-12-28 19:51:11 +0100

    yes

--- main.tf
+++ main.tf
@@ -24,4 +24,20 @@ provider "aws" {
 # create VPC
 resource "aws_vpc" "previtheque_vpc" {
     cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+      "Name" = "previtheque_vpc"
+    }
+}
+
+# Create subnets
+resource "aws_subnet" "publicsubnet1" {
+    vpc_id = aws_vpc.previtheque_vpc.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+      "Name" = "publicsubnet1"
+    }
+  
 }
