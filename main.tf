5508238792e169145de3dbaf93e00ae412744b9e
FranklinFoko <fokofranklin47@gmail.com>
2022-12-28 21:46:55 +0100

    yes

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "PrevithequeLBSecurityGroup" {
     description = "TCP"
     from_port = 80
     to_port = 80
-    protocol = "-1"
+    protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -95,6 +95,7 @@ resource "aws_security_group" "PrevithequeLBSecurityGroup" {
   egress {
     from_port = 0
     to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
