9ebe61380233f5666b6c105547836282c51b6e71
FranklinFoko <fokofranklin47@gmail.com>
2022-12-29 00:14:15 +0100

    yes

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ resource "aws_alb_target_group" "PrevithequeDevelopTargetGroup" {
 # Create alb listeners
 resource "aws_alb_listener" "listener_http" {
   load_balancer_arn = "arn:aws:elasticloadbalancing:eu-west-3:641144733479:loadbalancer/app/PrevithequeDevelopLB/a670cbc202d38ad5" 
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -172,6 +172,7 @@ resource "aws_alb_listener" "listener_https" {
   load_balancer_arn = "arn:aws:elasticloadbalancing:eu-west-3:641144733479:loadbalancer/app/PrevithequeDevelopLB/a670cbc202d38ad5"
   port              = 443
   protocol          = "HTTPS"
+  certificate_arn = "arn:aws:acm:eu-west-3:641144733479:certificate/f28702cf-df88-4a36-80f0-42cb725d5e6a"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
 
   default_action {
