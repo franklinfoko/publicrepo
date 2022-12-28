3768b0332507648237b5d07be920fb61ebe42d46
FranklinFoko <fokofranklin47@gmail.com>
2022-12-28 21:34:25 +0100

    yes

--- main.tf
+++ main.tf
@@ -78,13 +78,14 @@ resource "aws_security_group" "previtheque-sg" {
   ingress {
     from_port        = 0
     to_port          = 0
-    protocol         = -1
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
   ingress {
     from_port = 80
     to_port = 80
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
