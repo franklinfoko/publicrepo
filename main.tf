1a2972c97d9ed377b6f40dbb8f9a4e01f63c023b
FranklinFoko <fokofranklin47@gmail.com>
2022-12-28 20:27:01 +0100

    yes

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "franklinfoko"
 
     workspaces {
-        name = "example-workspace"
+        name = "publicrepo"
     }
   }  
   required_providers {
@@ -25,6 +25,9 @@ provider "aws" {
 resource "aws_vpc" "previtheque_vpc" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
+    enable_network_address_usage_metrics = false
+    enable_dns_hostnames = true
+    enable_dns_support = true
 
     tags = {
       "Name" = "previtheque_vpc"
@@ -35,9 +38,28 @@ resource "aws_vpc" "previtheque_vpc" {
 resource "aws_subnet" "publicsubnet1" {
     vpc_id = aws_vpc.previtheque_vpc.id
     cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
 
     tags = {
       "Name" = "publicsubnet1"
     }
-  
+}
+
+resource "aws_subnet" "publicsubnet2" {
+    vpc_id = aws_vpc.previtheque_vpc.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+      "Name" = "publicsubnet2"
+    }
+}
+
+resource "aws_subnet" "privatesubnet" {
+  vpc_id = aws_vpc.previtheque_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    "Name" = "privatesubnet"
+  }
 }
