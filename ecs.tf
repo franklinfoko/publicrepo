7b41dc101314f86e33433ddebcf06be7b6607d91
FranklinFoko <fokofranklin47@gmail.com>
2022-12-29 18:49:47 +0100

    yes

--- ecs.tf
+++ ecs.tf
@@ -18,13 +18,50 @@ data "template_file" "previthequeapp" {
 }
 
 resource "aws_ecs_task_definition" "PrevithequeTaskDefinitionDev" {
-  family = "previthequeapp-task"
+  family = "PrevithequeTaskDefinitionDev"
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = var.fargate_cpu
   memory = var.fargate_memory
-  #container_definitions = data.template_file.previthequeapp.rendered
+  container_definitions = jsonencode([
+    {
+      name      = "Api"
+      image     = "641144733479.dkr.ecr.eu-west-3.amazonaws.com/previtheque:develop"
+      cpu       = 0
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ],
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 0
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
 }
 
 resource "aws_ecs_service" "PrevithequeServiceDev" {
