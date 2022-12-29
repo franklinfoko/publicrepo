13435373e4b3a3247a316e80414eefd7d0926838
FranklinFoko <fokofranklin47@gmail.com>
2022-12-29 19:04:49 +0100

    yes

--- ecs.tf
+++ ecs.tf
@@ -53,15 +53,6 @@ resource "aws_ecs_task_definition" "PrevithequeTaskDefinitionDev" {
     }
   ])
 
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
 }
 
 resource "aws_ecs_service" "PrevithequeServiceDev" {
